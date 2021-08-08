7ef36107b1cea9e8d5457e690c984d5eaead0fff
123Nira <bhanu8759400@gmail.com>
2021-08-08 19:57:03 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,41 @@
+// TOPIC - HTML/CSS/JS
+// -center the box horizontally and vertically
+
+
+// -Draw a SVG Circle of Radius 20-30 m
+
+// -Create a anchor tag heading tag and paragraph tag and add LinkedIn link
+// -The content in heading tag should be underlined use border to do it
+// -Give styling to anchor heading and paragraph tag without using id and class
+// -Create a button and change its background color
+// - align text to center, change div background color, add hovering effects, creating links
+
+// Add two numbers using arrow function
+const add = (a, b) => {
+    return a + b;
+}
+let sum = add(3, 2)
+console.log(`Sum of  a and b is: ${sum}`);
+
+
+// How to add elements at the last of an array
+let arr = [3,5,6]
+arr.push(2)
+console.log(arr)
+arr[arr.length] = 7;
+console.log(arr)
+
+// Function to delete array elements in javascript.
+function delete_Any(arr) {
+    return arr.pop();
+    //return arr.shift()
+}
+let arr1 = [45,56,3,23]
+ console.log(delete_Any(arr1));
+
+// -How can we add elements at any specific position in JS?
+var arr2 = [];
+arr2[0] = "rwrwe";
+arr2[1] = "Tesla";
+arr2[2] = "Niraj";
+console.log(arr2);
